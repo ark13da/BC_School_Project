4eca1398662c5fbbd47279ceb07053cc74e6baf0
Keshmiry Alireza <ark13da@gmail.com>
2021-01-19 16:11:57 +0200

    19012021 js arrow function fixed

--- JavaScript/1_Task/script.js
+++ JavaScript/1_Task/script.js
@@ -1,12 +1,17 @@
 
 let age = prompt('How old are you?');
-//i have to learn the ternary use cases better
-//()=> { age <= 18 ? alert('You are too young!') : alert('welcome!') };
+//why the arrow below does not run? because i never called it. or alternatively i can use immediately invoked function(()=>)() 
+//()=> { return (age <= 18 ? alert('You are too young!') : alert('welcome!')); };
+(() => { return (age <= 18 ? alert('You are too young!') : alert('welcome!')); })();
+/*let validator = (i) => { return (age <= 18 ? alert('You are too young!') : alert('welcome!')); }
+validator(age);
+*/
+/*
 if (age <= 18) {
     alert('You are to young!');
 } else { 
     alert('welcome!');
-};
+};*/
 let firstNumber = prompt('Please enter a number:');
 let operator = prompt('Please enter a math operator:');
 let secondNumber = prompt('Please enter one more number:');
