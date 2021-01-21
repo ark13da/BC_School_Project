4cd30d788dbd684776700b4f48fe291a7153789e
Keshmiry Alireza <ark13da@gmail.com>
2021-01-21 09:45:09 +0200

    Finnish number generator

--- JavaScript/2_Task/script.js
+++ JavaScript/2_Task/script.js
@@ -3,23 +3,21 @@ let fiveArray = [];
 let fiveString = '';
 
 let fiver = () => {
-    for (let i = 0; i <5; i++) {
+    for (let i = 0; i < 5; i++) {
         fiveArray.push(Math.floor(Math.random() * Math.floor(2)));
     }
 
     if (fiveArray.includes(0) && fiveArray.includes(1)) {
-        fiveString = `The sequence was: ${fiveArray.join(',')} and you lost. Please try again!`;
+        fiveString = `The sequence was: ${fiveArray.join(',')} and you lost. Please try again! The chance of winning was ${1 / 2 ** 5} `;
     } else if (fiveArray.includes(0) && !fiveArray.includes(1)) {
-        fiveString = `The sequence was: ${fiveArray.join(',')} and you lost. But good job on getting all zeros; it is rare! Please try again!`;
+        fiveString = `The sequence was: ${fiveArray.join(',')} and you lost. But good job on getting all zeros; it is rare! Please try again! The chance of winning was ${1 / 2 ** 5} `;
     } else if (!fiveArray.includes(0) && fiveArray.includes(1)) {
-        fiveString = `The sequence was: ${fiveArray.join(',')} and you Won!`;
-    } else { 
+        fiveString = `The sequence was: ${fiveArray.join(',')} and you Won! The chance of winning was ${1 / 2 ** 5} `;
+    } else {
         fiveString = `The sequence was: ${fiveArray.join(',')} and this is a terrible code. please debug!`;
     }
-    
-    //console.log(fiveArray);
-    //console.log(fiveString);
-    document.getElementById('result').innerHTML=fiveString;
+
+    document.getElementById('result').innerHTML = fiveString;
     fiveArray = [];
     fiveString = '';
 };
--- JavaScript/3_Task/script.js
+++ JavaScript/3_Task/script.js
@@ -17,28 +17,64 @@ const nums = {
         7: 'seitsemän',
         8: 'kahdeksan',
         9: 'yhdeksan',
-        10: 'kummenen'
+        10: 'kymmenen'
 }
-//toista
-//kymmenttä
-//sataa
+
+
+let tens = (n) => { 
+    if (n - (Math.floor(n / 10) * 10) == 0) {
+        return nums[(Math.floor(n / 10) * 10) / 10] + 'kymmentä';
+    } else {
+        return nums[(Math.floor(n / 10) * 10) / 10] + 'kymmentä ' +
+            nums[n - (Math.floor(n / 10) * 10)];
+    };
+}
+
+let hundreds = (n) => { 
+
+    if (n - (Math.floor(n / 100) * 100) == 0) {
+        return nums[(Math.floor(n / 100) * 100) / 100] + ' sataa ';
+    } else if (n - (Math.floor(n / 100) * 100) <= 10) {
+        return nums[(Math.floor(n / 100) * 100) / 100] + ' sataa ' + nums[n - (Math.floor(n / 100) * 100)];
+    } else if (n - (Math.floor(n / 100) * 100) > 10 && (Math.floor(n / 100) * 100) < 20 ) {
+        return nums[(Math.floor(n / 100) * 100) / 100] + ' sataa ' + nums[(n - (Math.floor(n / 100) * 100)) - 10] + 'toista';
+    }else {
+        return nums[(Math.floor(n / 100) * 100) / 100] + ' sataa ' +
+            tens(n - (Math.floor(n / 100) * 100));
+    };
+}
+
 let numGenerator = () => {
     randNum = Math.round(Math.random() * 100) * Math.round(Math.random() * 10);
     
-    switch (randNum) { 
+    switch (true) { 
         case (randNum <= 10):
             finnTranslation = nums[randNum];
             break;
         case (randNum > 10 && randNum < 20):
             finnTranslation = nums[randNum - 10] + 'toista';
             break;
+        case (randNum > 19 && randNum < 100):
+            finnTranslation = tens(randNum);
+            break;
+            case (randNum == 100):
+                finnTranslation = 'sata';
+                break;
+        case (randNum > 100 && randNum < 1000):
+            finnTranslation = hundreds(randNum);
+            break;
+        case (randNum == 1000):
+            finnTranslation = 'tuhat';
+            break;
+        
         default: 
-            finnTranslation= "don't know yet"
+            finnTranslation= "Don't know yet. Please debug me!"
     }
+
+    
 
     document.getElementById('num').innerHTML = randNum;
     document.getElementById('translation').innerHTML = finnTranslation;
     randNum = 0;
     finnTranslation = '';
-    console.log(nums[8]);
 }
