42e4206f828c11a5dbaac4ba53a47930ab34fa78
Keshmiry Alireza <ark13da@gmail.com>
2021-02-16 14:28:39 +0200

    portfolio fixes

--- HTML5&CSS3/PersonalPortfolio/script.js
+++ HTML5&CSS3/PersonalPortfolio/script.js
@@ -1,13 +1,30 @@
 
+let header = document.getElementById("nav");
 
-let fadeOut = () => { 
-    //nav.style.backgroundColor ='rgba(86, 83, 107,0.2)'
-    //document.documentElement.scrollTop
-    if (document.documentElement.scrollTop > 1) {
+
+window.onscroll = function () { scrollFunction() };
+
+/*function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("nav").style.padding = "30px 10px";
         document.getElementById("nav").style.backgroundColor = "rgba(86, 83, 107,0.5)";
-        document.getElementById("nav").style.padding = "1%"}
-   
-    //document.getElementById("nav").style.backgroundColor = "rgba(86, 83, 107,0.5)";
-    //document.getElementById("nav").style.padding = "1%"
+        
+    } else {
+        document.getElementById("nav").style.padding = "40px 10px";
+        document.getElementById("nav").style.backgroundColor = "#FFC582";
+        
+    }
 }
-window.addEventListener("scroll", fadeOut);
+*/
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("nav").classList.add("band-scroll");
+        document.getElementById("btop").style.display = "block";
+        
+    } else {
+        document.getElementById("nav").classList.remove("band-scroll");
+        document.getElementById("btop").style.display = "none";
+        
+    }
+}
+
