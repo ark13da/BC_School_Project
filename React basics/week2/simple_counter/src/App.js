0a4b0ea2671e36c13611c2ad3a4e73a628baabdb
Keshmiry Alireza <ark13da@gmail.com>
2021-03-29 19:00:34 +0300

    react counter

---
+++ React basics/week2/simple_counter/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    counter: 1000
+  };
+
+  //evnt handlers
+  addHandler = () => {
+    this.setState(
+      { counter: this.state.counter + 1 }
+    );
+  };
+
+  removeHandler = () => {
+    if (this.state.counter>0){
+      this.setState(
+       { counter: this.state.counter - 1 }
+      );
+    }
+  };
+
+  resetHandler = () => {
+    this.setState(
+      { counter: 0 }
+    );
+  };
+
+  render() {
+    
+    let circleClass = `${this.state.counter === 0 ? "" : this.state.counter % 2 === 0 ? "even" : "odd"
+      } circle`;
+  return(
+    <div>
+      <h1 className={circleClass}>{this.state.counter}</h1>
+      <button onClick={ this.addHandler}>Add one</button>
+      <button onClick={this.removeHandler }>Remove one</button>
+      <button onClick={ this.resetHandler}>Reset</button>
+    </div>
+  );
+  }
+}
+
+export default App;
