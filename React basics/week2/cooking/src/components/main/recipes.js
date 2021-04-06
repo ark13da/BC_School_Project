d8df0f60e60cf6bc8de32e0fa61ee363a2d18acc
Keshmiry Alireza <ark13da@gmail.com>
2021-04-07 00:27:12 +0300

    cook book fixed

--- React basics/week2/cooking/src/components/main/recipes.js
+++ React basics/week2/cooking/src/components/main/recipes.js
@@ -8,46 +8,72 @@ class Recipes extends Component {
     state = {
         data: []
     };
+
+
     
-    componentDidMount() {
+    fillTheStore() {
         const url = "https://api.spoonacular.com/recipes/random?apiKey=" + config.recipeKey;
             fetch(url).then(res => {
                 if (res.ok) {
-                    //console.log(res);
+                
                     return res.json();
                 } else {
                     console.log('error');
                 }
             })
                 .then(json => {
+                    
                     this.setState({
-                        data: [...this.state.data,json.recipes[0]]
-                    })
-                    //result = json.recipes[0];
-                    //randomRecipe.push(result);
+                        
+                        data: [...this.state.data, json.recipes[0]]
+                    });
+
                     //image
                     //summary
                     //instructions
                     //[array]extendedIngredients.originalString
                     //sourceUrl
-                    //console.log(result);
-                    //console.log(result.image);
-                    //randomRecipe.push(<div className="recipe"><img className="thumbnail" src={result.image} alt="foodPic" /><h3>{result.title}</h3></div>);
                 })
                 .catch(err => {
                     console.log('error');
                 })
     }
 
+   /* 
+   //coverting strigified array back to array is challenging 
+   componentDidMount() {
+        if (this.storage.getItem('storedRecipes') === null || !JSON.parse(this.storage.getItem('storedRecipes')).length){
+            for (let i = 0; i < 3; i++) {
+            this.fillTheStore();
+            }
+            this.storage.setItem('storedRecipes', this.recipeList.toString());
+            this.setState({
+            data: JSON.parse(this.storage.getItem('storedRecipes'))
+            });
+        }
+
+    }*/
+    componentDidMount() {
+        if (!this.state.data.length) {
+            for (let i = 0; i < 4; i++) {
+                this.fillTheStore();
+            }
+            
+        }
+
+    } 
+
+
     render() {
+        
         return (
             <div className="recipesContent">
-                {this.state.data.map(item=>(
-                    <div className="recipe">
+                {this.state.data.map(item=>
+                    <div className="recipe" key={ item.id}>
                         <img className="thumbnail" src={item.image} alt="foodPic" />
-                        <h3>{item.title}</h3>
+                        <h4>{item.title}</h4>
                     </div>
-                ))}
+                )}
             </div>
         );
     }
