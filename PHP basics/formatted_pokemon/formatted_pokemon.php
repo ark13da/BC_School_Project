c817f330e76e3761239d8e0d8c07313e5dfe510a
Keshmiry Alireza <ark13da@gmail.com>
2021-04-16 18:40:42 +0300

    add pokemons midway

--- PHP basics/formatted_pokemon/formatted_pokemon.php
+++ PHP basics/formatted_pokemon/formatted_pokemon.php
@@ -1,9 +1,25 @@
 <?php
     $data = file_get_contents('pokemon.json');
-
+    
     // echo '<pre>';
     $formatted_data = json_decode($data, true);
     $results = $formatted_data['results'];
+
+    //adding new pokemons
+    if (isset($_POST['name'])){
+        $id_regex= '/(' . $_POST['id'] .'\/)$/';
+        for ($i = 0; $i < count($results); $i++){
+            if(strtoupper($_POST['name'])==$results[$i]['name'] || preg_match($id_regex, $results[$i]['url'])){
+                echo 'no go';
+                return;
+            }else{
+                echo 'good to go';
+                return;
+            }
+        
+        }
+    }
+
     $chunked_results= array_chunk($results,50);
     $formatted_results = array();
     $page_number = 0;
@@ -17,6 +33,7 @@
         // $results[$i]['name'] = strtoupper($results[$i]['name']); Notice that this is illegal 
         $formatted_results[$i]['name'] = strtoupper($chunked_results[$page_number][$i]['name']);
         $formatted_results[$i]['url'] = $chunked_results[$page_number][$i]['url'];
+        
     }
 
     
---
+++ PHP basics/formatted_pokemon/new_pokemon.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pokemon form</title>
+</head>
+<body>
+     <form action="formatted_pokemon.php" method="POST">
+         <input type="text" name="id" placeholder="ID">
+         <br>
+         <input type="text" name="name" placeholder="Name">
+         <br>
+         <select name="type">
+             <option value="normal">Normal</option>
+             <option value="fire">Fire</option>
+             <option value="water">Water</option>
+         </select>
+         <br>
+         <input type="submit" value="Add Pokemon">
+     </form>
+   
+</body>
+</html>
