74c485614a5c3c55e893cce0363edea98b86d80e
Keshmiry Alireza <ark13da@gmail.com>
2021-05-04 19:57:09 +0300

    recipe search half done

--- React basics/week5/functional_recipe/src/components/main/recipes.js
+++ React basics/week5/functional_recipe/src/components/main/recipes.js
@@ -3,55 +3,77 @@ import "./recipes.css";
 import RecipeCard from "./RecipeCard.js";
 import SingleRecipe from "./SingleRecipe.js";
 import axios from "axios";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { Switch, Route, useRouteMatch, useHistory } from "react-router-dom";
 
 
 const Recipes =()=> {
   let { path, url } = useRouteMatch();
   const [recipeList, setRecipeList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [searchTerm, setSearchTerm] = useState();
-  const [searchExists,setSearchExists] = useState(false);
-  
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchExists, setSearchExists] = useState("");
+  let history = useHistory();
+
   const changeHandler = (e) => {
     setSearchTerm(e.target.value);
-  }
+  };
+
   const searchHandler = () => {
-    if (searchTerm) {
+    setSearchExists(searchTerm);
+    setSearchTerm("");
+    document.getElementById("search").value = "";
+  };
+  const clearHandler = () => {
+    setSearchExists('');
+    setSearchTerm('');
+  };
+
+  const matchSearch = () => {
+    if (searchExists.length) {
       let searchMatch = recipeList.filter(
-        (i) => i.title.toLowerCase() === searchTerm.toLowerCase()
+        (i) => i.title.toLowerCase() === searchExists.toLowerCase()
       );
-      if (searchMatch.length) {
-        setRecipeList(searchMatch);
-        setSearchExists(true);
-      }
+      setRecipeList(searchMatch);
+      document.getElementById("search").innerHTML='';
     }
   };
-  
+
   useEffect(() => {
     setIsLoading(true);
-    axios.get("http://localhost:3001/recipes").then((res) => {
-      setRecipeList(res.data);
-    });
-    
+    axios
+      .get("http://localhost:3001/recipes")
+      .then((res) => {
+        setRecipeList(res.data);
+      })
+      .then((res) => matchSearch());
     setIsLoading(false);
   }, [searchExists]);
 
   if (isLoading) {
-    return (
-      <div className="recipesContent">
-      Loading...
-      </div>
-    );
+    return <div className="recipesContent">Loading...</div>;
   }
+  /*
   if (!recipeList.length) {
     return (
       <div className="marginTop">
-        No match for search term
+        <input
+          type="text"
+          id="search"
+          placeholder="Search recipe"
+          onChange={changeHandler}
+        />
+        <button type="button" onClick={searchHandler}>
+          Search
+        </button>
+        <button type="button" onClick={clearHandler}>
+          clear Search
+        </button>
+        <p>No match for search term</p>
+        <button onClick={() => history.goBack()}>Go back</button>
       </div>
     );
   }
-
+*/
   return (
     <div className="marginTop">
       <input
@@ -60,7 +82,12 @@ const Recipes =()=> {
         placeholder="Search recipe"
         onChange={changeHandler}
       />
-      <button type="button" onClick={searchHandler}>Search</button>
+      <button type="button" onClick={searchHandler}>
+        Search
+      </button>
+      <button type="button" onClick={clearHandler}>
+        Clear search
+        </button>
       <div className="recipesContent">
         <Switch>
           <Route path="/recipes" exact>
@@ -72,6 +99,7 @@ const Recipes =()=> {
                 title={item.title}
               />
             ))}
+            
           </Route>
           <Route path={`${path}/:id`}>
             <SingleRecipe />
@@ -80,7 +108,6 @@ const Recipes =()=> {
       </div>
     </div>
   );
-  
 }
 
 export default Recipes;
