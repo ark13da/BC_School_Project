7f0a16ba92df3e10fff6c9eb6e8ceff2bfa347de
Keshmiry Alireza <ark13da@gmail.com>
2021-08-02 16:10:17 +0300

    recipe hashed

--- React basics/week5/functional_recipe/src/App.js
+++ React basics/week5/functional_recipe/src/App.js
@@ -7,7 +7,7 @@ import About from "./components/main/about.js"
 import Recipes from "./components/main/recipes.js"
 import Newrecipe from "./components/main/Newrecipe.js";
 
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
